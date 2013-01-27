9e2649a871582032ebfdad32550b557700e4eac9
Martin Gross <martin@pc-coholic.de>
2013-01-27 15:45:15 +0100

    Modify code to use hex/strings instead of ints

--- MFV.py
+++ MFV.py
@@ -1,24 +1,34 @@
 import serial
+import sys
 
 class MFV(object):
 	def __init__(self, serialport = '/dev/ttyUSB0'):
 		self.__ser = serial.Serial(serialport, baudrate=9600, dsrdtr=True)
-		self.__DLE = 10
-		self.__STX = 2
-		self.__ETX = 3
-		self.__ENQ = 5
-		self.__ACK = 6
-		self.__NAK = 15
-		self.__EOT = 4
-		self.__CMD = 43
+		self.__DLE = '0x10'
+		self.__STX = '0x02'
+		self.__ETX = '0x03'
+		self.__ENQ = '0x05'
+		self.__ACK = '0x06'
+		self.__NAK = '0x15'
+		self.__EOT = '0x04'
+		self.__CMD = '0x43'
+		self.__POS = '0x50'
+		self.__NEG = '0x4E'
 
 	def reset(self):
-		command = [self.__CMD, "0", "0"]
+		command = [self.__CMD, '0', '0']
 		if self.send(command) == True:
 			return self.enquire()
 		else:
 			return False
-	
+
+	def waitforinsert(self):
+		command = [self.__CMD, ':', '0']
+		if self.send(command) == True:
+			return self.enquire()
+		else:
+			return False
+
 	def enquire(self):
 		return self.send([self.__ENQ])
 
@@ -27,19 +37,20 @@ class MFV(object):
 			command = [self.__DLE] + command
 		else:
 			# convert all strings to hex
-			for i in range(len(command)):
+			for i in range(1, len(command)):
 				if type(command[i]) == type(str()):
-					command[i] = int(command[i].encode("hex"))
-	
+					command[i] = str(command[i].encode("hex"))
+
+
 			bcc = self.bcc(command)
-			
+
 			command = [self.__DLE, self.__STX] + command + [self.__DLE, self.__ETX, bcc]
-
+	
 		commandstr = ''
-		for arg in command:
-			commandstr += chr(int("0x" + str(arg), 16))
-			
-
+		for i in range(len(command)):
+			command[i] = command[i][-2:]
+			commandstr += chr(int("0x" + str(command[i]), 16))
+		
 		print("OUT: " + str(command))
 		self.__ser.write(commandstr)
 		return self.read()
@@ -49,21 +60,34 @@ class MFV(object):
 		exp = ""
 		response = self.__ser.read(2)
 		response = self.arrayify_response(response)
-		
-		if response[1] == self.__ACK:
-			exp = "ACK"
-			ret = True
-		elif response[1] == self.__STX:
-			moreresponse = [0, 0]
-			while moreresponse != [self.__DLE, self.__ETX]:
-				moreresponse[0] = moreresponse[1]
-				tmpresponse = self.arrayify_response(self.__ser.read(1))
-				response += tmpresponse
-				moreresponse[1] = tmpresponse[0]
-			ret = True
-			exp = "ACK; Response follows"
+
+		if response[0] == self.__DLE:
+			if response[1] == self.__ACK:
+				exp = "ACK"
+				ret = True
+			elif response[1] == self.__STX:
+				moreresponse = [0, 0]
+				while moreresponse != [self.__DLE, self.__ETX]:
+					moreresponse[0] = moreresponse[1]
+					tmpresponse = self.arrayify_response(self.__ser.read(1))
+					response += tmpresponse
+					moreresponse[1] = tmpresponse[0]
+				# Still need to read the bcc
+				response += self.arrayify_response(self.__ser.read(1))
+				
+				if response[2] == self.__POS:
+					ret = True
+					exp = "ACK; POSITIVE; Response follows"
+				elif response[2] == self.__NEG:
+					ret = True
+					exp = "ACK; NEGATIVE; Response follows"
+				else:
+					ret = False
+					exp = "ACK; UNKNOWN; Response follows"
+			else:
+				print("Received unknown reponse-state " + str(response[1]))
 		else:
-			print("Received unknown reponse-state " + str(response[1]))
+			exp = "Some error occured"
 
 		print("IN:  " + str(response) + " " + exp)
 		return ret
@@ -73,13 +97,14 @@ class MFV(object):
 		
 		bcc = 0
 		for arg in command:
-			bcc = bcc^arg
+			bcc = bcc^int(arg, 16)
 		
-		return bcc
+		return hex(bcc)
 
 	def arrayify_response(self, response):
 		array = []
 		for i in range( 0, len(response) ):
-			array += [int("".join([str(hex(ord(x)))[2:] for x in response[i]]))]
-
+			#array += [str("".join([str(hex(ord(x)))[2:] for x in response[i]]))]
+			array += ["0x%0.2X" % ord(response[i])]
+			 
 		return array
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import MFV
+import time
 
 reader = MFV.MFV('/dev/ttyUSB3')
 
+print(reader.reset())
+print(reader.waitforinsert())
+time.sleep(5)
 print(reader.reset())
